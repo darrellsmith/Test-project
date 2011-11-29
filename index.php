8677dc1b3db786f9eafc0a520ab2b682a52faf27
unknown <Darrell@.(none)>
2011-11-29 13:55:52 +0000

    First Commit of test file

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Git Test</title>
+
+    </head>
+    <body>
+        <div style="text-align: center">
+            <h1>GIT test site</h1>
+        </div>
+        <div style="text-align: left">
+            <p>this is a test file</p>
+            <?php
+                echo date('G:ia  D M Y')
+            ?>            
+        </div>
+    </body>
+</html>
+
