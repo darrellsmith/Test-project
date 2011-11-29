159f795b134d3385ca7a9b9797903b406dc0cc3f
unknown <Darrell@.(none)>
2011-11-29 14:06:44 +0000

    Added page render time

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+$time = microtime();
+
+$time = explode(" ", $time);
+
+$time = $time[1] + $time[0];
+
+$start = $time;
+
+?>
 <html>
     <head>
         <title>Git Test</title>
@@ -10,7 +20,21 @@
         <div style="text-align: left">
             <p>this is a test file</p>
             <?php
-                echo date('G:ia  D M Y')
+                echo date('G:ia  D M Y');
+            
+                $time = microtime();
+
+                $time = explode(" ", $time);
+
+                $time = $time[1] + $time[0];
+
+                $finish = $time;
+
+                $totaltime = ($finish - $start);
+                
+                echo("<br/><br/>");
+                
+                echo ("This page took ".round($totaltime,4) ."seconds to load.");
             ?>            
         </div>
     </body>
